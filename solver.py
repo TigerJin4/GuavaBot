ed66ac19a075b87cabf1c189769b4d431166f33b
Wenhan Jin <tigerjin4@berkeley.edu>
2019-04-26 00:05:51 -0700

    hello wendy

--- solver.py
+++ solver.py
@@ -37,35 +37,40 @@ def solve(client):
         for i in range(num_iterations):
             frumNode = topological_order[i]
             toNode = predecessor[frumNode]
-            if cannotscout[frumNode]:
-                pass
-            scout_result = np.array(list(client.scout(frumNode, all_students).values()))
+            can_update_mistake = False
+            if not cannotscout[frumNode]:
+                can_update_mistake = True
+                scout_result = np.array(list(client.scout(frumNode, all_students).values()))
             remote_result = client.remote(frumNode, toNode)
             cannotscout[frumNode] = True
-            cannotscout[toNode] = True
-            print('scout result is', scout_result)
+            if remote_result > 0:
+                cannotscout[toNode] = True
             if remote_result == 0:
-                students_correctness = (scout_result == remote_result)
-                mistake = mistake + (1 - students_correctness)
+                if can_update_mistake:
+                    students_correctness = (scout_result == remote_result)
+                    mistake = mistake + (1 - students_correctness)
             else:
-                students_correctness = (scout_result == 1)
-                mistake = mistake + (1 - students_correctness)
+                if can_update_mistake:
+                    students_correctness = (scout_result == 1)
+                    mistake = mistake + (1 - students_correctness)
                 must_have[toNode] += remote_result
 
     def phaseTwo(cutoff):
         ## Input: integer cutoff point for the confidence that the target point has a bot
         nonlocal numIterations, mistake, cannotscout
-        for i in range(numIterations, len(topological_order)):
+        for i in range(numIterations, len(topological_order)-1):
             currentNode = topological_order[i]
             toNode = predecessor[currentNode]
-            if cannotscout[currentNode]:
-                pass
-            scout_result = np.array(list(client.scout(currentNode, all_students).values()))
+            can_update_mistake = False
+            if not cannotscout[currentNode]:
+                can_update_mistake = True
+                scout_result = np.array(list(client.scout(currentNode, all_students).values()))
             ## max_num = max(mistake)
             if must_have[currentNode] > 0:
                 remote_result = client.remote(currentNode, toNode)
                 cannotscout[currentNode] = True
-                cannotscout[toNode] = True
+                if remote_result > 0:
+                    cannotscout[toNode] = True
                 must_have[toNode] += remote_result
                 mistake = mistake + (scout_result == False)
                 pass
@@ -74,12 +79,15 @@ def solve(client):
                 if sum(scout_result * weighted_authority) >= cutoff:
                     remote_result = client.remote(currentNode, toNode)
                     if remote_result == 0:
-                        students_correctness = (scout_result == remote_result)
-                        mistake = mistake + (1 - students_correctness)
+                        if can_update_mistake:
+                            students_correctness = (scout_result == remote_result)
+                            mistake = mistake + (1 - students_correctness)
                     else:
-                        students_correctness = (scout_result == 1)
-                        mistake = mistake + (1 - students_correctness)
+                        if can_update_mistake:
+                            students_correctness = (scout_result == 1)
+                            mistake = mistake + (1 - students_correctness)
                         must_have[toNode] += remote_result
+                        cannotscout[toNode] = True
 
     numIterations = len(topological_order) // 2
     cutpoint = 0.5
